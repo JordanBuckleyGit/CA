7c3bffa7b178de3966eb772ba8509bae38625407
JordanBuckleyGit <jordanbuckleycork@gmail.com>
2025-02-19 18:37:40 +0000

    adding session

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
 import random
 from database import get_db, close_db
+from flask_session import Session
 from forms import MovieForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "this"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 app.teardown_appcontext(close_db)
 
 @app.route("/")
